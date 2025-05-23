f8c2327b76e7af26010c7492093b189130b69c4b
Matus <matuskocik@gmail.com>
2025-05-23 18:00:25 +0200

    Add article translation test

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
+from wagtail.models import Locale
 
 from .models import Article
 
@@ -7,10 +9,22 @@ class ArticleListView(ListView):
     model = Article
     template_name = "articles/articles.html"
     context_object_name = "articles"
-    queryset = Article.objects.all().order_by("-date_published")
+
+    def get_queryset(self):
+        current_locale = Locale.get_active()
+        return (
+            Article.objects.live()
+            .filter(locale=current_locale)
+            .order_by("-date_published")
+        )
 
 
 class ArticleDetailView(DetailView):
     model = Article
     template_name = "articles/article.html"
     context_object_name = "article"
+
+    def get_object(self, queryset=None):
+        slug = self.kwargs.get("slug")
+        locale = Locale.get_active()
+        return get_object_or_404(Article.objects.live(), slug=slug, locale=locale)
--- config/urls.py
+++ config/urls.py
@@ -16,17 +16,25 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.i18n import set_language
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from articles import urls as articles_urls
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("cms/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("", include(articles_urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path("set-language/", set_language, name="set_language"),
+]
